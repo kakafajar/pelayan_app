1d2cde57155fb1c7d74c4bcce6c79141ba61bd0a
irfan-ubpk23 <if23.muhamadfauzi@mhs.ubpkarawang.ac.id>
2025-06-29 17:32:07 +0700

    menyambungkan api ke halaman menu dan mempindahkan css didalam page.html ke page.scss

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,8 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MENU_ITEMS } from '../data/menu';
-import { Params } from '@angular/router';
 import { ToastController } from '@ionic/angular';
+import { MenuService } from '../service/menu.service';
+import { SingletonService } from '../service/singleton.service';
+import { KategoriService } from '../service/kategori.service';
+import { OrderService } from '../service/order.service';
+import { OrderDetailService } from '../service/order-detail.service';
+import { TransaksiService } from '../service/transaksi.service';
+import { lastValueFrom } from 'rxjs';
+
+
+interface cartItem{
+  menu:any;
+  jumlah:number;
+}
 
 @Component({
   standalone: false,
@@ -11,85 +22,104 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
-  nomorMeja: any;
   kategoriDipilih = 'special';
-  jumlahDipilih: number = 0;
   totalHarga: number = 0;
 
-  daftarMenu = MENU_ITEMS;
-  filteredMenu = [...MENU_ITEMS];
+  searchTerm :string="";
 
-  keranjang: any[] = [];
-  daftarPesanan: any[] = [];
+  kategoriList:any[]=[];
+  selectedMeja :any;
 
+  menuList:any[]=[];
+  menuFilteredIds:any[]=[];
 
+  cartList: {[index:string]:cartItem}= {};
+  cartTotalCount:number=0;
   metodePembayaran: string = '';
-
 
   // 🔽 Variabel untuk halaman Rincian Pesanan
   showRincianPesanan = false;
   showPesananAktif = true;
-
   
-  kategoriPesanan: string = 'dinein';
-  namaPelanggan = '';
-  jumlahTamu = 2;
+  jenisLayanan:string="";
 
-  constructor(private route: ActivatedRoute, private router: Router, private toastController: ToastController) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router, 
+    private toastController: ToastController,
+    private ngZone:NgZone,
 
-
-
- tampilkanPesananAktif() {
-  this.showRincianPesanan = false;
-  this.showPesananAktif = true;
-
-}
-
-filterMenu() {
-  const category = this.kategoriDipilih;
-  this.filteredMenu = this.daftarMenu.filter(item => item.category === category);
-}
-
-pilihKategori(kat: string) {
-  this.kategoriDipilih = kat;
-  this.filterMenu();
-}
-
-
-
+    private menuService: MenuService,
+    private kategoriService: KategoriService,
+    private orderService:OrderService,
+    private orderDetailService:OrderDetailService,
+    private transaksiService:TransaksiService,
+    private singletonService:SingletonService
+  ) {}
 
   ngOnInit() {
-  this.route.queryParams.subscribe((params: Params) => {
-    this.nomorMeja = params['meja'];
-    this.kategoriPesanan = params['jenisLayanan'];
+    this.jenisLayanan = this.singletonService.temps["jenisLayanan"];
+    this.selectedMeja = this.singletonService.temps['selectedMeja'];
+    this.singletonService.clearTemps();
+
+    this.ngZone.run(()=>{
+      this.menuService.all()
+      .subscribe(response=>{
+        this.menuList.push(...response.data);
+      })
+  
+      this.kategoriService.all()
+      .subscribe(response=>{
+        this.kategoriList.push(...response.data);
+      })
+    })
+  }
+
+  tampilkanPesananAktif() {
+    this.showRincianPesanan = false;
+    this.showPesananAktif = true;
+  }
+
+  filterMenu() {
+    const category = this.kategoriDipilih;
+    // this.filteredMenu = this.daftarMenu.filter(item => item.category === category);
+  }
+
+  pilihKategori(kat: string) {
+    this.kategoriDipilih = kat;
     this.filterMenu();
-  });
-}
+  }
 
   // 🔍 Pencarian
   cariMenu(event: any) {
-    const keyword = event.detail.value?.toLowerCase() || '';
-    this.filteredMenu = this.daftarMenu.filter(item =>
-      item.name.toLowerCase().includes(keyword) ||
-      item.description.toLowerCase().includes(keyword)
-    );
+    this.searchTerm = event.target.value?.toLowerCase() || '';
+    this.menuFilteredIds = [];
+    this.menuList.forEach((menu)=>{
+      if(menu.nama_menu.toLowerCase().includes(this.searchTerm)){
+        this.menuFilteredIds.push(menu.id);
+        
+      }
+    });
   }
 
   // ➕ Tambah ke keranjang
-  tambahKeKeranjang(item: any) {
-    const index = this.keranjang.findIndex(i => i.id === item.id);
-    if (index >= 0) {
-      this.keranjang[index].jumlah += 1;
-    } else {
-      this.keranjang.push({ ...item, jumlah: 1 });
+  tambahKeKeranjang(menu: any) {
+    if (this.cartList.hasOwnProperty(menu.id)){
+      this.cartList[menu.id].jumlah += 1;
+      this.cartTotalCount += 1;
+    }else{
+      this.cartList[menu.id] = {menu: menu, jumlah: 1};
+      this.cartTotalCount = 1;
     }
     this.updateRingkasan();
   }
 
   // 🔄 Update ringkasan (jumlah & total)
   updateRingkasan() {
-    this.jumlahDipilih = this.keranjang.reduce((sum, item) => sum + item.jumlah, 0);
-    this.totalHarga = this.keranjang.reduce((sum, item) => sum + item.jumlah * item.price, 0);
+    this.totalHarga = 0;
+    for (let key in this.cartList){
+      this.totalHarga += this.cartList[key].menu.harga_menu * this.cartList[key].jumlah;
+    }
   }
 
   // 👉 Lanjut ke rincian pesanan
@@ -104,105 +134,104 @@ pilihKategori(kat: string) {
   }
 
   // ➕➖ Ubah jumlah item di rincian
-  ubahJumlah(item: any, perubahan: number) {
-    const index = this.keranjang.findIndex(i => i.id === item.id);
-    if (index >= 0) {
-      this.keranjang[index].jumlah += perubahan;
-      if (this.keranjang[index].jumlah < 1) {
-        this.keranjang[index].jumlah = 1;
-      }
-      this.updateRingkasan();
-    }
+  ubahJumlah(menu_id: any, perubahan: number) {
+    this.cartList[menu_id].jumlah += perubahan;
+    this.updateRingkasan();
   }
 
   // 🗑️ Hapus item dari keranjang
-  hapusItem(item: any) {
-    const index = this.keranjang.findIndex(i => i.id === item.id);
-    if (index >= 0) {
-      this.keranjang.splice(index, 1);
-    }
+  hapusItem(menu_id: any) {
+    delete this.cartList[menu_id];
     this.updateRingkasan();
   }
 
   // 👤 Tambah/kurang tamu
   ubahJumlahTamu(delta: number) {
-    this.jumlahTamu = Math.max(1, this.jumlahTamu + delta);
+    // this.jumlahTamu = Math.max(1, this.jumlahTamu + delta);
   }
 
   // ❌ Batalkan seluruh pesanan
   batalkanPesanan() {
-    this.keranjang = [];
-    this.namaPelanggan = '';
-    this.jumlahTamu = 2;
+    this.cartList = {};
+    this.cartTotalCount = 0;
+    this.totalHarga = 0;
     this.showRincianPesanan = false;
-    this.updateRingkasan();
+    // this.updateRingkasan();
   }
 
   // 💾 Simpan pesanan
-  // 💾 Simpan pesanan
-async simpanPesanan() {
-  if (!this.metodePembayaran) {
-    const toast = await this.toastController.create({
-      message: 'Silakan pilih metode pembayaran terlebih dahulu.',
-      color: 'warning',
-      duration: 2500,
-      position: 'top',
-      icon: 'alert-circle-outline',
-    });
-    await toast.present();
-    return;
+  async simpanPesanan() {
+    if (!this.metodePembayaran) {
+      const toast = await this.toastController.create({
+        message: 'Silakan pilih metode pembayaran terlebih dahulu.',
+        color: 'warning',
+        duration: 2500,
+        position: 'top',
+        icon: 'alert-circle-outline',
+      });
+      await toast.present();
+      return;
+    }
+    
+    try{
+      let order:any;
+      const orderRequest$ = this.orderService.create({
+        user_id : localStorage.getItem("user_id"),
+        jenis_order: this.jenisLayanan
+      });
+      
+      await lastValueFrom(orderRequest$).then(response=>{
+        order = response.data;
+      });
+
+      // creatine order detail
+      for (let key in this.cartList){
+        const item = this.cartList[key]
+        const orderDetail$ = this.orderDetailService.create({
+          order_id : order.id,
+          menu_id : item.menu.id,
+          jumlah : item.jumlah
+        });
+        
+        await lastValueFrom(orderDetail$).catch(error=>{
+          console.log(error);
+        });
+      }
+
+      let transaksi:any;
+      const transaksi$ = this.transaksiService.create({
+        user_id : localStorage.getItem("user_id"),
+        order_id : order.id,
+        metode_pembayaran: this.metodePembayaran,
+        total_harga : this.totalHarga
+      });
+
+      await lastValueFrom(transaksi$).then(response=>{
+        transaksi = response.data;
+      }).catch(error=>{
+        console.log(error);
+      });
+
+      const toast = await this.toastController.create({
+        message: 'Pesanan berhasil dikirim! Order #001235 telah masuk ke dapur.',
+        color: 'success',
+        duration: 3000,
+        position: 'top',
+        icon: 'checkmark-circle-outline',
+      });
+
+      await toast.present();
+
+      this.singletonService.temps = {
+        lastTransaksi : transaksi
+      }
+      this.router.navigate(['/konfirmasi']);
+    }
+    catch(error){
+      alert(error);
+      console.log(error);
+    }
   }
-const now = new Date();
-const jam = now.toLocaleString('id-ID', {
-  day: '2-digit',
-  month: 'short',
-  year: 'numeric',
-  hour: '2-digit',
-  minute: '2-digit',
-  hour12: false
-});
-
-const dataPesanan = {
-  meja: this.nomorMeja,
-  nama: this.namaPelanggan,
-  jumlahTamu: this.jumlahTamu,
-  item: this.keranjang,
-  total: this.totalHarga,
-  metode: this.metodePembayaran,
-  jenisLayanan: this.kategoriPesanan,
-  jam: jam,
-  statusPembayaran: 'Belum Bayar'
-};
-
-
-  console.log('✅ Pesanan disimpan:', dataPesanan);
-
-  const toast = await this.toastController.create({
-    message: 'Pesanan berhasil dikirim! Order #001235 telah masuk ke dapur.',
-    color: 'success',
-    duration: 3000,
-    position: 'top',
-    icon: 'checkmark-circle-outline',
-  });
-
-  await toast.present();
-
-    // Simpan ke localStorage
-  // console.log(dataPesanan);
-  
-  localStorage.setItem('pesananTerakhir', JSON.stringify(dataPesanan));
-
-  // Navigasi ke halaman konfirmasi sambil mengirim data (optional: stringify jika objek besar)
-  this.router.navigate(['/konfirmasi'], {
-    queryParams: {
-      meja: this.nomorMeja,
-      metode: this.metodePembayaran,
-      total: this.totalHarga,
-      jenisLayanan:this.kategoriPesanan
-    },
-    state: { pesanan: dataPesanan } // cara kirim full object
-  });
-}
 
 // Contoh fungsi ketika user bayar
 tandaiSudahBayar() {
